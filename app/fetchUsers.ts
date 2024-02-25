4d8e1522675f5e3cb48775cd8c34e3f025422433
Nathan Drake <nathandrakedrums@gmail.com>
2024-02-25 09:38:15 -0600

    ready to start

---
+++ app/fetchUsers.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const fetchUsers = async (amount: number) => {
+  const res = await axios.get(`http://localhost:3005/users?amount=${amount}`);
+
+  return res.data;
+};
