a0260044b0ee1da7115cf1b46d4f32d1a5d5fbc5
Nathan Drake <nathandrakedrums@gmail.com>
2024-02-28 15:19:02 -0600

    final starting point

--- app/fetchUsers.ts
+++ app/fetchUsers.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const fetchUsers = async (amount: number) => {
-  const res = await axios.get(`http://localhost:3005/users?amount=${amount}`);
+  const res = await axios.get(
+    `http://localhost:3005/users?amount=${amount}`,
+  );
 
   return res.data;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { fetchUsers } from "./fetchUsers";
 
 export default async function Home() {
-  const users = await fetchUsers(20);
+  const users = await fetchUsers(1);
 
-  const getZodiacSignMessage = (zodiacSign: string) => {
-    return `Your zodiac sign is ${zodiacSign}.`;
+  const getZodiacSignMessage = (zodiac: string) => {
+    return `Your zodiac sign is ${zodiac}.`;
   };
 
   const getTailwindReaction = (tailwind: string) => {
     if (tailwind === "love") return "😍";
     if (tailwind === "haet") return "😡";
+  };
+
+  const getColorMessage = (color: string) => {
+    return `Your favorite color is ${color}.`;
   };
 
   return (
@@ -29,14 +33,14 @@ export default async function Home() {
               <h2 className="text-xl font-bold">Name: {user.name}</h2>
               <p className="text-gray-500">Email: {user.email}</p>
               <p className="text-gray-500">
-                {getZodiacSignMessage(user.zodiacSign)}
+                {getZodiacSignMessage(user.zodiac)}
               </p>
               <p className="text-gray-500">
                 Tailwind: {getTailwindReaction(user.tailwind)}
               </p>
-              <p className="text-gray-500">
-                Favorite Color: {user.favoriteColor}
-              </p>
+              {/* <p className="text-gray-500">
+                {getColorMessage(user.favoriteColor)}
+              </p> */}
             </div>
           );
         })}
