13b4ca3f3c94b746c8c14d89a0ced4aee3dcba9c
Nathan Drake <ndrake@paylocity.com>
2024-05-23 13:08:40 -0500

    post presentation

--- app/fetchUsers.ts
+++ app/fetchUsers.ts
@@ -1,9 +1,33 @@
-import axios from "axios";
+import { z } from "zod";
+
+export type HexColor = `#${string}`;
+
+const hexColorSchema = z.custom<HexColor>((value) => {
+  return /^#[0-9A-F]{6}$/i.test(value);
+});
+
+export const userSchema = z.object({
+  email: z.string().email(),
+  favoriteColor: hexColorSchema,
+  id: z.number(),
+  name: z.string(),
+  tailwind: z.enum(["love", "hate"]),
+  zodiac: z.string().optional(),
+});
+
+export type User = z.infer<typeof userSchema>;
 
 export const fetchUsers = async (amount: number) => {
-  const res = await axios.get(
+  const res = await fetch(
     `http://localhost:3005/users?amount=${amount}`,
+    {
+      cache: "no-store",
+    },
   );
 
-  return res.data;
+  const data: unknown = await res.json();
+
+  const users = userSchema.array().parse(data);
+
+  return users;
 };
